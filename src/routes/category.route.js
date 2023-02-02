40cf7baeeb1d5640c4b4c82e4af108a83fdffe97
Bostan-MERN_Dev <bostan.khan801@gmail.com>
2023-02-02 17:18:03 +0500

    bug fixing

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const CategoryController = require("../controllers/category.controller");
 const router = express.Router();
-
-
-
 router.get("/", CategoryController.getCategory);
 router.get("/:id", CategoryController.getCategoryByID)
 router.post("/", CategoryController.createCategory);
